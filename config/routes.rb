3f9d5791461510b375c80a741cdec3d3b73828dc
Daniel Jackson <jacksonvoice@yahoo.com>
2014-10-18 08:30:51 -0700

    delted some unused views, added the outline for courses_controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :sections
 
-  resources :courses
+  resources :courses do
+    resources :sections
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
